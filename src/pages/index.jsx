a37e70b9b4c3e8ea8c72d611a75cf93776386284
Davidandrade01 <david.andrade01@live.com>
2023-01-16 12:13:17 +0000

    corrections

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.scss'
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 export default function Home() {
@@ -8,7 +9,8 @@ export default function Home() {
     <><title>My List</title>
     <div className={styles.home}>
     
-     <img src="./imageedit_2_6686559213.png" alt="my List-image" width={600} />
+    <Image src='/image/mylist.png' width={700} height={500} alt={"MyList-todo"} />
+
      <div className={styles.brand}>
        <h1>Plan, edit, create, set goals.
        Make the most of your day Use <b>My LIST</b></h1>
